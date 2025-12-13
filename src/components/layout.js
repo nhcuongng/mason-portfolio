d1a17093020ad06003b92371975d66fc4c151111
Cuong Nguyen Huu <cuongnh@fint.vn>
2025-12-14 00:52:33 +0700

    fix: bring the loader back

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import styled, { ThemeProvider } from 'styled-components';
-import { Head, Nav, Social, Email, Footer } from '@components';
+import { Head, Loader, Nav, Social, Email, Footer } from '@components';
 import { GlobalStyle, theme } from '@styles';
 
 const StyledContent = styled.div`
@@ -12,6 +12,7 @@ const StyledContent = styled.div`
 
 const Layout = ({ children, location }) => {
   const isHome = location.pathname === '/';
+  const [isLoading, setIsLoading] = useState(isHome);
 
   // Sets target="_blank" rel="noopener noreferrer" on external links
   const handleExternalLinks = () => {
@@ -27,6 +28,10 @@ const Layout = ({ children, location }) => {
   };
 
   useEffect(() => {
+    if (isLoading) {
+      return;
+    }
+
     if (location.hash) {
       const id = location.hash.substring(1); // location.hash without the '#'
       setTimeout(() => {
@@ -39,7 +44,7 @@ const Layout = ({ children, location }) => {
     }
 
     handleExternalLinks();
-  }, []);
+  }, [isLoading]);
 
   return (
     <>
@@ -53,18 +58,7 @@ const Layout = ({ children, location }) => {
             Skip to Content
           </a>
 
-          <StyledContent>
-            <Nav isHome={isHome} />
-            <Social isHome={isHome} />
-            <Email isHome={isHome} />
-
-            <div id="content">
-              {children}
-              <Footer />
-            </div>
-          </StyledContent>
-
-          {/* {isLoading && isHome ? (
+          {isLoading && isHome ? (
             <Loader finishLoading={() => setIsLoading(false)} />
           ) : (
             <StyledContent>
@@ -77,7 +71,7 @@ const Layout = ({ children, location }) => {
                 <Footer />
               </div>
             </StyledContent>
-          )} */}
+          )}
         </ThemeProvider>
       </div>
     </>
