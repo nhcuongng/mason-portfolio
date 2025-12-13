ab421071ec3768fbac6eef009b577744a04e9288
Cuong Nguyen Huu <cuongnh@fint.vn>
2025-12-13 17:18:05 +0700

    fix: add install gasby

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,11 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Mason Nguyen',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Mason Nguyen is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
+    siteUrl: 'http://nhcuong.cloud', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
